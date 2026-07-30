1c5b1b363422e6aa85dcefd2e364f5647bec8ab2
ceydankara <ceydanka209@gmail.com>
2026-07-30 14:56:16 +0300

    .

--- src/main/java/com/example/defecttracker/repository/DefectRepository.java
+++ src/main/java/com/example/defecttracker/repository/DefectRepository.java
@@ -1,9 +1,7 @@
 package com.example.defecttracker.repository;
-
 import com.example.defecttracker.entity.Defect;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
-
 public interface DefectRepository extends JpaRepository<Defect, Long> {
     Optional<Defect> findByCoilId(String coilId);
 }
