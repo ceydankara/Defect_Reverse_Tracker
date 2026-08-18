2ea77d54d8d9fbaea0e10e4c370d78e3c71fc559
ceydankara <ceydanka209@gmail.com>
2026-08-18 21:54:30 +0300

    .

--- src/main/java/com/example/defecttracker/dto/FieldCaseResolutionRequestDto.java
+++ src/main/java/com/example/defecttracker/dto/FieldCaseResolutionRequestDto.java
@@ -5,6 +5,8 @@ import lombok.Data;
 @Data
 public class FieldCaseResolutionRequestDto {
     private String commercialAction;
+    /** Telafi (kredi/red vb.) ile birlikte CAPA kaydı açılsın mı */
+    private Boolean openCapa;
     private String capaReference;
     private String resolutionNotes;
     private Boolean markResolved;
--- src/main/java/com/example/defecttracker/dto/RemediationPlanDto.java
+++ src/main/java/com/example/defecttracker/dto/RemediationPlanDto.java
@@ -12,4 +12,8 @@ public class RemediationPlanDto {
     private String dominantLabel;
     private List<String> workflowSteps;
     private List<RemediationOptionDto> options;
+    /** Telafi seçimine ek olarak CAPA açılabilir (üretim / lojistik) */
+    private boolean capaOptional;
+    private String capaLabel;
+    private boolean capaDefaultOpen;
 }
